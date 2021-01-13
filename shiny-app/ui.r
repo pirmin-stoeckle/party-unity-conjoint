b3233963b1a505e6c9d91716065d98b35919b27f
Pirmin Stöckle <57797210+pirmin-stoeckle@users.noreply.github.com>
2021-01-13 13:38:20 +0100

    testing shiny_example template

--- shiny-app/ui.r
+++ shiny-app/ui.r
@@ -1,64 +1,25 @@
-shinyUI(fluidPage(
-  useShinyjs(),
-  
-  titlePanel("Party Unity Vote Experiment - Simulation Tool"),
-  
-  theme = shinytheme("flatly"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      "This could be a description of how all this works"
-   ),
+shinyUI(bootstrapPage(
 
- mainPanel(
-   fluidRow(
-     column(3, h2("Candidate 1"),
-            div(
-            id = "scenario_1",
-            h4("Candidate characteristics"),
-            selectInput('gender_1', 'Gender', choices = levels(linpreds$gender)),
-            selectInput('age_1', 'Age', choices = levels(linpreds$age)),
-            selectInput('job_1', 'Former occupation', choices = levels(linpreds$job)),
-            h4("Intra-party behavior"),    
-            selectInput('conference_1', 'Behavior at party conference', choices = levels(linpreds$conference)),
-            selectInput('parliament_1', 'Voting behavior in parliament', choices = levels(linpreds$parliament)),
-            selectInput('critique_1', 'Intra-party critique', choices = levels(linpreds$critique)),
-            selectInput('reform_1', 'Clarity of reform proposals', choices = levels(linpreds$reform)),
-            h4("Party position"), 
-            selectInput('dist_1', 'Ideological distance', choices = levels(linpreds$dist)),
-            h4("Party's characteristic"), 
-            selectInput('role_1', 'Party role', choices = levels(linpreds$role)),
-            h4("Respondent's characteristic"), 
-            selectInput('respondent_educ_1', "Respondent's education", choices = levels(linpreds$respondent_educ)),
-            actionButton("reset_1", "Reset")
-            )
-     ),
-     column(3, h2("Candidate 2"),
-            div(
-            id = "scenario_2",
-            h4("Candidate characteristics"),
-            selectInput('gender_2', 'Gender', choices = levels(linpreds$gender)),
-            selectInput('age_2', 'Age', choices = levels(linpreds$age)),
-            selectInput('job_2', 'Former occupation', choices = levels(linpreds$job)),
-            h4("Intra-party behavior"),    
-            selectInput('conference_2', 'Behavior at party conference', choices = levels(linpreds$conference)),
-            selectInput('parliament_2', 'Voting behavior in parliament', choices = levels(linpreds$parliament)),
-            selectInput('critique_2', 'Intra-party critique', choices = levels(linpreds$critique)),
-            selectInput('reform_2', 'Clarity of reform proposals', choices = levels(linpreds$reform)),
-            h4("Party position"), 
-            selectInput('dist_2', 'Ideological distance', choices = levels(linpreds$dist)),
-            h4("Party's characteristic"), 
-            selectInput('role_2', 'Party role', choices = levels(linpreds$role)),
-            h4("Respondent's characteristic"), 
-            selectInput('respondent_educ_2', "Respondent's education", choices = levels(linpreds$respondent_educ)),
-            actionButton("reset_2", "Reset")
-            )
-     ),
-     column(5, h2("Predicted vote probabilities"),
-            plotOutput("plot"),
-            tableOutput("table")
-     )
-   ),
- )
+  selectInput(inputId = "n_breaks",
+              label = "Number of bins in histogram (approximate):",
+              choices = c(10, 20, 35, 50),
+              selected = 20),
+
+  checkboxInput(inputId = "individual_obs",
+                label = strong("Show individual observations"),
+                value = FALSE),
+
+  checkboxInput(inputId = "density",
+                label = strong("Show density estimate"),
+                value = FALSE),
+
+  plotOutput(outputId = "main_plot", height = "300px"),
+
+  # Display this only if the density is shown
+  conditionalPanel(condition = "input.density == true",
+    sliderInput(inputId = "bw_adjust",
+                label = "Bandwidth adjustment:",
+                min = 0.2, max = 2, value = 1, step = 0.2)
   )
+
 ))
