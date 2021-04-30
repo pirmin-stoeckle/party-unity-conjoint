54a38009cf7263ce71fd6c03f113a341140ee012
sjuhl <sebastian.juhl@gess.uni-mannheim.de>
2021-04-30 15:51:03 +0200

    update distplot_critique2

--- plots.R
+++ plots.R
@@ -281,46 +281,18 @@ dev.off()
 
 
 
-
-# Effect of ideological distance by reform clarity
-dist_by_reform <- scenarios[role == "government party (not PM party)" &
-                                  conference == "united" &
-                                  parliament == "united" &
-                                  critique == "rank-and-file members" &
-                                  reform %in% c("low", "high") &
-                                  gender == "female" &
-                                  age == "38y" &
-                                  job == "employee" &
-                                  dist %in% c(0:4),]
-  
-
-# #plot
-# distplot_reform <- ggplot(dist_by_reform, aes(x = prob, y = dist)) +
-#   geom_pointrange(aes(xmin = lower, xmax = upper), size = 0.25) +
-#   facet_wrap(vars(reform)) +
-#   ggtitle("Clarity of reform proposals") +
-#   ylab("Ideological distance") +
-#   xlab("Predicted vote share versus baseline") +
-#   coord_flip() +
-#   theme_bw()
-# 
-# #save plot to pdf
-# pdf(file=paste0(getwd(),"/figures/distplot_reform.pdf"), width = 7, height = 3)
-# distplot_reform
-# dev.off()
-
 # Effect of ideological distance by internal critique
-dist_by_critique <- scenarios[role == "government party (not PM party)" &
-                              conference == "united" &
-                              parliament == "united" &
-                              critique %in% c("rank-and-file members"
-                                              ,"former party leader"
-                                              ,"party faction"
-                                              ,"none") &
-                              reform == "high" &
-                              gender == "female" &
-                              age == "38y" &
-                              job == "employee" &
+dist_by_critique <- scenarios[role == "Junior coalition partner" &
+                              conference == "United" &
+                              parliament == "United voting" &
+                              critique %in% c("Rank-and-file members"
+                                              ,"Former party leader"
+                                              ,"Party faction"
+                                              ,"None") &
+                              reform == "High" &
+                              gender == "Female" &
+                              age == "38 years" &
+                              job == "Employee" &
                               dist %in% c(0:4),]
 
 
@@ -341,7 +313,7 @@ dist_by_critique <- scenarios[role == "government party (not PM party)" &
 
 
 
-# ALTERNATIVE PLOT
+# PLOT
 pdf(file=paste0(getwd(),"/figures/distplot_critique2.pdf"),width=7, height=3)
 par(oma=c(0,0,1,0),mar=c(3,3,0,0))
 plot(0,xlim=c(.5,5.5),ylim=c(0,50)
@@ -354,47 +326,47 @@ abline(v=c(0.5,1.5,2.5,3.5,4.5),col="white",lwd=13)
 for(i in as.numeric(unique(dist_by_critique$dist))){
   # rank and file
   points(x=i-.15
-         ,y=dist_by_critique$prob[dist_by_critique$critique=="rank-and-file members"
+         ,y=dist_by_critique$prob[dist_by_critique$critique=="Rank-and-file members"
                                     & as.numeric(dist_by_critique$dist)==i]
          ,pch=20
          ,cex=.8)
   lines(x=c(i-.15,i-.15)
-        ,y=c(dist_by_critique$lower[dist_by_critique$critique=="rank-and-file members"
+        ,y=c(dist_by_critique$lower[dist_by_critique$critique=="Rank-and-file members"
                                       & as.numeric(dist_by_critique$dist)==i]
-             ,dist_by_critique$upper[dist_by_critique$critique=="rank-and-file members"
+             ,dist_by_critique$upper[dist_by_critique$critique=="Rank-and-file members"
                                        & as.numeric(dist_by_critique$dist)==i]))
   # none
   points(x=i-.05
-         ,y=dist_by_critique$prob[dist_by_critique$critique=="none"
+         ,y=dist_by_critique$prob[dist_by_critique$critique=="None"
                                   & as.numeric(dist_by_critique$dist)==i]
          ,pch=4
          ,cex=.8)
   lines(x=c(i-.05,i-.05)
-        ,y=c(dist_by_critique$lower[dist_by_critique$critique=="none"
+        ,y=c(dist_by_critique$lower[dist_by_critique$critique=="None"
                                     & as.numeric(dist_by_critique$dist)==i]
-             ,dist_by_critique$upper[dist_by_critique$critique=="none"
+             ,dist_by_critique$upper[dist_by_critique$critique=="None"
                                      & as.numeric(dist_by_critique$dist)==i]))
   # former leader
   points(x=i+.05
-         ,y=dist_by_critique$prob[dist_by_critique$critique=="former party leader"
+         ,y=dist_by_critique$prob[dist_by_critique$critique=="Former party leader"
                                     & as.numeric(dist_by_critique$dist)==i]
          ,pch=17
          ,cex=.8)
   lines(x=c(i+.05,i+.05)
-        ,y=c(dist_by_critique$lower[dist_by_critique$critique=="former party leader"
+        ,y=c(dist_by_critique$lower[dist_by_critique$critique=="Former party leader"
                                       & as.numeric(dist_by_critique$dist)==i]
-             ,dist_by_critique$upper[dist_by_critique$critique=="former party leader"
+             ,dist_by_critique$upper[dist_by_critique$critique=="Former party leader"
                                        & as.numeric(dist_by_critique$dist)==i]))
   # faction
   points(x=i+.15
-         ,y=dist_by_critique$prob[dist_by_critique$critique=="party faction"
+         ,y=dist_by_critique$prob[dist_by_critique$critique=="Party faction"
                                   & as.numeric(dist_by_critique$dist)==i]
          ,pch=18
          ,cex=.8)
   lines(x=c(i+.15,i+.15)
-        ,y=c(dist_by_critique$lower[dist_by_critique$critique=="party faction"
+        ,y=c(dist_by_critique$lower[dist_by_critique$critique=="Party faction"
                                     & as.numeric(dist_by_critique$dist)==i]
-             ,dist_by_critique$upper[dist_by_critique$critique=="party faction"
+             ,dist_by_critique$upper[dist_by_critique$critique=="Party faction"
                                      & as.numeric(dist_by_critique$dist)==i]))
   
 }
@@ -407,6 +379,7 @@ mtext("Ideological Distance",side=1,line=2)
 legend("topright",pch=c(20,4,17,18),legend=c("Rank-and-File","None","Former Leader","Faction")
        ,cex=.8,bg="white")
 dev.off()
+
 
 
 
